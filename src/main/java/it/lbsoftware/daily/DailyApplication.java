98c4050e3952ec563043d7d63018a378eb4d9a39
Luca Bonetti <lb.luca.bonetti@gmail.com>
2021-03-08 22:35:46 +0100

    Add Spring Boot project initial files

---
+++ src/main/java/it/lbsoftware/daily/DailyApplication.java
@@ -0,0 +1,13 @@
+package it.lbsoftware.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/lbsoftware/daily/DailyApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lbsoftware.daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
