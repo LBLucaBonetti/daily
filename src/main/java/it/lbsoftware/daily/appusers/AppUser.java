7d7e27e82be3f3736f2d25fd53a4f218cc9d26ca
Luca Bonetti <lb.luca.bonetti@gmail.com>
2021-04-06 23:25:17 +0200

    feat: add basic user entity

---
+++ src/main/java/it/lbsoftware/daily/appusers/AppUser.java
@@ -0,0 +1,35 @@
+package it.lbsoftware.daily.appusers;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NotBlank
+    private String uid;
+    @Email
+    @NotNull
+    private String email;
+    private LocalDateTime registrationDateTime;
+    private LocalDateTime lastAccessDateTime;
+
+}
