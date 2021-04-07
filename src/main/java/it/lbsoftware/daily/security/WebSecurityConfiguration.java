d1e02fad50ce09d7eb8e83b136e9d8015ba82b94
Luca Bonetti <lb.luca.bonetti@gmail.com>
2021-04-08 00:04:48 +0200

    feat: user services only exposed via its service class

--- src/main/java/it/lbsoftware/daily/security/WebSecurityConfiguration.java
+++ src/main/java/it/lbsoftware/daily/security/WebSecurityConfiguration.java
@@ -2,13 +2,18 @@ package it.lbsoftware.daily.security;
 
 import com.okta.spring.boot.oauth.Okta;
 import it.lbsoftware.daily.appusers.AppUserRegistrationFilter;
+import it.lbsoftware.daily.appusers.AppUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final AppUserService appUserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,7 +27,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .cors();
         http
-                .addFilterAfter(new AppUserRegistrationFilter(), BearerTokenAuthenticationFilter.class);
+                .addFilterAfter(new AppUserRegistrationFilter(appUserService), BearerTokenAuthenticationFilter.class);
 
         Okta.configureResourceServer401ResponseBody(http);
     }
