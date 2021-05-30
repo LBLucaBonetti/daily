89671de41dbe19387b72b5f24ff76b40d41cd6b0
Luca Bonetti <lb.luca.bonetti@gmail.com>
2021-05-30 12:21:30 +0200

    refactor: change user retrieval and http status codes when entity is not found

--- src/main/java/it/lbsoftware/daily/tags/TagController.java
+++ src/main/java/it/lbsoftware/daily/tags/TagController.java
@@ -5,9 +5,7 @@ import it.lbsoftware.daily.appusers.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -23,24 +21,16 @@ class TagController {
     private final TagService tagService;
 
     @PostMapping
-    public ResponseEntity<Tag> createTag(@Valid @RequestBody Tag tag, JwtAuthenticationToken jwtAuthenticationToken) {
-        Optional<AppUser> appUserOptional = appUserService.getAppUserFromToken(jwtAuthenticationToken);
-        if (appUserOptional.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User could not be found");
-        }
-        AppUser appUser = appUserOptional.get();
+    public ResponseEntity<Tag> createTag(@Valid @RequestBody Tag tag) {
+        AppUser appUser = appUserService.getAppUserFromToken();
         Tag createdTag = tagService.createTag(tag, appUser);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTag);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tag> readTag(@PathVariable("id") Long id, JwtAuthenticationToken jwtAuthenticationToken) {
-        Optional<AppUser> appUserOptional = appUserService.getAppUserFromToken(jwtAuthenticationToken);
-        if (appUserOptional.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User could not be found");
-        }
-        AppUser appUser = appUserOptional.get();
+    public ResponseEntity<Tag> readTag(@PathVariable("id") Long id) {
+        AppUser appUser = appUserService.getAppUserFromToken();
         Optional<Tag> readTag = tagService.readTag(id, appUser);
         if (readTag.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -50,29 +40,21 @@ class TagController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Tag> updateTag(@PathVariable("id") Long id, @Valid @RequestBody Tag tag, JwtAuthenticationToken jwtAuthenticationToken) {
-        Optional<AppUser> appUserOptional = appUserService.getAppUserFromToken(jwtAuthenticationToken);
-        if (appUserOptional.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User could not be found");
-        }
-        AppUser appUser = appUserOptional.get();
+    public ResponseEntity<Tag> updateTag(@PathVariable("id") Long id, @Valid @RequestBody Tag tag) {
+        AppUser appUser = appUserService.getAppUserFromToken();
         Optional<Tag> updatedTag = tagService.updateTag(id, tag, appUser);
         if (updatedTag.isEmpty()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.notFound().build();
         }
 
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Tag> deleteTag(@PathVariable("id") Long id, JwtAuthenticationToken jwtAuthenticationToken) {
-        Optional<AppUser> appUserOptional = appUserService.getAppUserFromToken(jwtAuthenticationToken);
-        if (appUserOptional.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User could not be found");
-        }
-        AppUser appUser = appUserOptional.get();
+    public ResponseEntity<Tag> deleteTag(@PathVariable("id") Long id) {
+        AppUser appUser = appUserService.getAppUserFromToken();
         if (!tagService.deleteTag(id, appUser)) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.notFound().build();
         }
 
         return ResponseEntity.noContent().build();
