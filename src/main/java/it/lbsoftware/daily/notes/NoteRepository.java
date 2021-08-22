7f8689848287225cfaf34ef9aec7a9e460583ff6
Luca Bonetti <lb.luca.bonetti@gmail.com>
2021-08-22 15:10:53 +0200

    feat: add query methods to get notes from db

--- src/main/java/it/lbsoftware/daily/notes/NoteRepository.java
+++ src/main/java/it/lbsoftware/daily/notes/NoteRepository.java
@@ -1,8 +1,11 @@
 package it.lbsoftware.daily.notes;
 
+import it.lbsoftware.daily.appusers.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
@@ -13,5 +16,22 @@ public interface NoteRepository extends JpaRepository<Note, Long> {
      * @return Found notes or empty list
      */
     List<Note> findByTextContaining(String contained);
+
+    /**
+     * Finds a note by its id and AppUser
+     *
+     * @param uuid    Note uuid
+     * @param appUser User
+     * @return Found note or empty value
+     */
+    Optional<Note> findByUuidAndAppUser(UUID uuid, AppUser appUser);
+
+    /**
+     * Finds notes by their AppUser
+     *
+     * @param appUser User
+     * @return Found notes or empty list
+     */
+    List<Note> findByAppUser(AppUser appUser);
 
 }
