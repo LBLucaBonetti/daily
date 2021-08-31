e81513ae99c0af2b78f72b181f8b6defd45ea1da
Luca Bonetti <lb.luca.bonetti@gmail.com>
2021-09-01 00:10:04 +0200

    feat: remove unused global method security

--- src/main/java/it/lbsoftware/daily/DailyApplication.java
+++ src/main/java/it/lbsoftware/daily/DailyApplication.java
@@ -3,10 +3,8 @@ package it.lbsoftware.daily;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableAsync
 public class DailyApplication {
 
