dbf56e260c12ea32cd5a37bc179ea28a1b1096fd
Luca <lb.luca.bonetti@gmail.com>
2021-12-14 18:11:16 +0100

    refactor: rename note service tests class

--- src/test/java/it/lbsoftware/daily/notes/NoteServiceImplTests.java
+++ src/test/java/it/lbsoftware/daily/notes/NoteServiceImplTests.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-class NoteServiceImplTest {
+class NoteServiceImplTests {
 
     @Mock
     private NoteRepository noteRepository;
