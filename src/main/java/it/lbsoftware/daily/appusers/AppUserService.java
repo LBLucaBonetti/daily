419f8cf8bd0af23ea65887732acd1e0875652ca8
Luca <lb.luca.bonetti@gmail.com>
2021-12-19 22:37:57 +0100

    refactor: remove unused import, edit comments

--- src/main/java/it/lbsoftware/daily/appusers/AppUserService.java
+++ src/main/java/it/lbsoftware/daily/appusers/AppUserService.java
@@ -1,23 +1,22 @@
 package it.lbsoftware.daily.appusers;
 
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 public interface AppUserService {
 
     /**
-     * Creates a new user if it does not exist, updates its information otherwise; details are stored as Authentication details
+     * Creates a new app user if it does not exist, updates its information otherwise; details are stored as Authentication details
      *
-     * @param jwtAuthenticationToken Token that provides user details and to set user details to
+     * @param jwtAuthenticationToken Token that provides app user details and to set app user details to
      */
     void setAppUserToToken(JwtAuthenticationToken jwtAuthenticationToken);
 
     /**
-     * Gets the request user from the token
+     * Gets the request app user from the token
      *
-     * @return Found user or empty value
+     * @return Found app user or empty value
      */
     Optional<AppUser> getAppUserFromToken();
 
