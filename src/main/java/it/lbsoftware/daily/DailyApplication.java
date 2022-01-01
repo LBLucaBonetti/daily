58d2946542fa4ebbdb42755118b280f267834c96
Luca <lb.luca.bonetti@gmail.com>
2022-01-01 18:00:32 +0100

    feat: remove unused async annotation for the whole app

--- src/main/java/it/lbsoftware/daily/DailyApplication.java
+++ src/main/java/it/lbsoftware/daily/DailyApplication.java
@@ -2,10 +2,8 @@ package it.lbsoftware.daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class DailyApplication {
 
     public static void main(String[] args) {
