e2d973d6dd01dd3798a88463301e693e45766cd6
Luca <lb.luca.bonetti@gmail.com>
2022-09-18 16:40:43 +0200

    fix: db migration not populating data before adding not null constraint

--- src/main/resources/db/migration/V1_1_0__remove_appuser.sql
+++ src/main/resources/db/migration/V1_1_0__remove_appuser.sql
@@ -1,22 +1,22 @@
 -- Add new app_user column to note and tag
+-- Copy from app_user.uid to note.app_user and tag.app_user
 ALTER TABLE note ADD COLUMN app_user VARCHAR(255);
 
-ALTER TABLE note ALTER COLUMN app_user SET NOT NULL;
-
-ALTER TABLE tag ADD COLUMN app_user VARCHAR(255);
-
-ALTER TABLE tag ALTER COLUMN app_user SET NOT NULL;
-
--- Copy from app_user.uid to note.app_user and tag.app_user
 UPDATE note SET app_user = (
   SELECT au.uid FROM note n1 JOIN app_user au ON n1.app_user_id = au.id
   WHERE n1.id = note.id
 );
 
+ALTER TABLE note ALTER COLUMN app_user SET NOT NULL;
+
+ALTER TABLE tag ADD COLUMN app_user VARCHAR(255);
+
 UPDATE tag SET app_user = (
   SELECT au.uid FROM tag t1 JOIN app_user au ON t1.app_user_id = au.id
   WHERE t1.id = tag.id
 );
+
+ALTER TABLE tag ALTER COLUMN app_user SET NOT NULL;
 
 -- Drop app_user table and related connections
 ALTER TABLE note DROP CONSTRAINT FK_NOTE_ON_APPUSERID;
