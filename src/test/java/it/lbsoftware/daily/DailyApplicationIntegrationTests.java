d4945951895458d92e354efe904ae50eb9affef4
Luca <lb.luca.bonetti@gmail.com>
2022-10-02 18:48:40 +0200

    test: main class case

---
+++ src/test/java/it/lbsoftware/daily/DailyApplicationIntegrationTests.java
@@ -0,0 +1,17 @@
+package it.lbsoftware.daily;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("DailyApplication integration tests")
+class DailyApplicationIntegrationTests extends DailyAbstractIntegrationTests {
+
+  @Test
+  @DisplayName("Should not throw when the application starts")
+  void test1() {
+    DailyApplication.main(new String[] {});
+    assertTrue(true);
+  }
+}
