cd2d8144ae1649ca95e1dcb2500d94a3069e65a3
Luca <lb.luca.bonetti@gmail.com>
2022-10-03 13:37:58 +0200

    fix: profiles were not passed to the main class integration test

--- src/test/java/it/lbsoftware/daily/DailyApplicationIntegrationTests.java
+++ src/test/java/it/lbsoftware/daily/DailyApplicationIntegrationTests.java
@@ -11,7 +11,7 @@ class DailyApplicationIntegrationTests extends DailyAbstractIntegrationTests {
   @Test
   @DisplayName("Should not throw when the application starts")
   void test1() {
-    DailyApplication.main(new String[] {});
+    DailyApplication.main(new String[] {"--spring.profiles.active=test,okta"});
     assertTrue(true);
   }
 }
