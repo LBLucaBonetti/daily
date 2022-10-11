4af8f5c1bb1ce76e1a3b07b6af5adf953200aaba
Luca <lb.luca.bonetti@gmail.com>
2022-10-11 22:46:50 +0200

    feat: add app user controller

---
+++ src/main/java/it/lbsoftware/daily/appusers/AppUserController.java
@@ -0,0 +1,10 @@
+package it.lbsoftware.daily.appusers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/appusers")
+public class AppUserController {}
