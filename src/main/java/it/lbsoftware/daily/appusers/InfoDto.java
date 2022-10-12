614866a33b96b3e6bbd384be8730e0da43af3cdc
Luca <lb.luca.bonetti@gmail.com>
2022-10-12 09:46:24 +0200

    feat: app user information dto is now a record

--- src/main/java/it/lbsoftware/daily/appusers/InfoDto.java
+++ src/main/java/it/lbsoftware/daily/appusers/InfoDto.java
@@ -1,14 +1,3 @@
 package it.lbsoftware.daily.appusers;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@EqualsAndHashCode
-public class InfoDto {
-
-  private String fullName;
-  private String email;
-}
+public record InfoDto(String fullName, String email) {}
