e63de6d598e405a4a10b805509086df68a7462d4
Luca <lb.luca.bonetti@gmail.com>
2022-10-12 09:48:05 +0200

    feat: remove public modifier from app user controller

--- src/main/java/it/lbsoftware/daily/appusers/AppUserController.java
+++ src/main/java/it/lbsoftware/daily/appusers/AppUserController.java
@@ -7,4 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/appusers")
-public class AppUserController {}
+class AppUserController {}
