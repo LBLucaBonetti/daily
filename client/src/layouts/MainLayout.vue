4248d3d79d93524e4275dc91181d2ce9238b0803
Luca <lb.luca.bonetti@gmail.com>
2022-10-12 19:09:32 +0200

    feat: add app user information to the top of the left panel drawer

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -18,16 +18,27 @@
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label class="poppins-regular" header> daily </q-item-label>
-        <q-item
-          ><q-btn
-            flat
-            dense
-            :ripple="false"
-            @click="logout()"
-            :icon="heroOutline24ArrowRightOnRectangle"
-          ></q-btn>
-          <form hidden :id="logoutFormId" method="post" action="/logout"></form
-        ></q-item>
+        <q-item class="justify-between items-center">
+          <div>
+            <q-item-label>{{ fullName }}</q-item-label>
+            <q-item-label>{{ email }}</q-item-label>
+          </div>
+          <div>
+            <q-btn
+              flat
+              dense
+              :ripple="false"
+              @click="logout()"
+              :icon="heroOutline24ArrowRightOnRectangle"
+            ></q-btn>
+            <form
+              hidden
+              :id="logoutFormId"
+              method="post"
+              action="/logout"
+            ></form>
+          </div>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -38,12 +49,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import {
   heroOutline24Bars3,
   heroOutline24ArrowRightOnRectangle,
 } from 'quasar-extras-svg-icons/hero-icons-v2';
 import { Cookies } from 'quasar';
+import { api } from 'src/boot/axios';
+import { AxiosResponse } from 'axios';
 
 const leftDrawerOpen = ref(false);
 const logoutFormId = 'logout-form';
@@ -70,4 +83,18 @@ function logout() {
   // Submit the form, effectively logout
   form.submit();
 }
+
+const fullName = ref('');
+const email = ref('');
+
+interface Info {
+  fullName: string;
+  email: string;
+}
+onMounted(() => {
+  api.get('/appusers/info').then((res: AxiosResponse<Info>) => {
+    fullName.value = res.data.fullName;
+    email.value = res.data.email;
+  });
+});
 </script>
