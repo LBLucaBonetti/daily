b2e7c5b7d3a220db072ee4161a9a3627b6ff6df6
Luca <lb.luca.bonetti@gmail.com>
2022-10-12 13:29:46 +0200

    test: add app user integration tests class

---
+++ src/test/java/it/lbsoftware/daily/appusers/AppUserIntegrationTests.java
@@ -0,0 +1,9 @@
+package it.lbsoftware.daily.appusers;
+
+import it.lbsoftware.daily.DailyAbstractIntegrationTests;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("AppUser integration tests")
+class AppUserIntegrationTests extends DailyAbstractIntegrationTests {
+  private static final String BASE_URL = "/api/appusers";
+}
