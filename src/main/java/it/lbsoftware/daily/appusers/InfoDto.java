bc7dc55c3573ba5e2038000d6dd85a7f775b338e
Luca <lb.luca.bonetti@gmail.com>
2022-10-12 09:33:12 +0200

    feat: add app user information dto

---
+++ src/main/java/it/lbsoftware/daily/appusers/InfoDto.java
@@ -0,0 +1,14 @@
+package it.lbsoftware.daily.appusers;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class InfoDto {
+
+  private String fullName;
+  private String email;
+}
