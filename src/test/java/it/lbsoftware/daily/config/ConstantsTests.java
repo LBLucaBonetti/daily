5b334315ebc2d918ef71c361c890533eca4f7334
Luca <lb.luca.bonetti@gmail.com>
2022-10-25 21:55:55 +0200

    test: add missing case for constants class constructor

---
+++ src/test/java/it/lbsoftware/daily/config/ConstantsTests.java
@@ -0,0 +1,33 @@
+package it.lbsoftware.daily.config;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import it.lbsoftware.daily.DailyAbstractUnitTests;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Constants unit tests")
+class ConstantsTests extends DailyAbstractUnitTests {
+
+  @Test
+  @DisplayName("Should not instantiate the class")
+  void test1() throws NoSuchMethodException {
+    // Given
+    Constructor<Constants> constantsConstructor = Constants.class.getDeclaredConstructor();
+    assertTrue(Modifier.isPrivate(constantsConstructor.getModifiers()));
+    constantsConstructor.setAccessible(true);
+
+    // When
+    InvocationTargetException res =
+        assertThrows(InvocationTargetException.class, constantsConstructor::newInstance);
+
+    // Then
+    assertNotNull(res);
+    assertTrue(res.getCause() instanceof UnsupportedOperationException);
+  }
+}
