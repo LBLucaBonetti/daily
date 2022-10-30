e6678efca5bbb4752ea7b3f9f32d3f1367fc2e3d
Luca <lb.luca.bonetti@gmail.com>
2022-10-30 11:55:11 +0100

    test: add missing page dto case

---
+++ src/test/java/it/lbsoftware/daily/bases/PageDtoTests.java
@@ -0,0 +1,27 @@
+package it.lbsoftware.daily.bases;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import it.lbsoftware.daily.DailyAbstractUnitTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+
+@DisplayName("PageDto unit tests")
+class PageDtoTests extends DailyAbstractUnitTests {
+
+  @Test
+  @DisplayName("Should throw when create page dto with null page")
+  void test1() {
+    // Given
+    Page<Object> page = null;
+
+    // When
+    IllegalArgumentException res =
+        assertThrows(IllegalArgumentException.class, () -> new PageDto<>(page));
+
+    // Then
+    assertNotNull(res);
+  }
+}
