cf195127637a08b8c1538131728557c2637f3379
Luca <lb.luca.bonetti@gmail.com>
2022-11-12 19:03:02 +0100

    feat: add error handling to imports in routes in order to refresh session

--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -3,15 +3,29 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () =>
+      import('layouts/MainLayout.vue')
+        .then((mainLayout) => mainLayout)
+        .catch(() => (window.location.href = window.location.href)),
+    children: [
+      {
+        path: '',
+        component: () =>
+          import('pages/IndexPage.vue')
+            .then((indexPage) => indexPage)
+            .catch(() => (window.location.href = window.location.href)),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () =>
+      import('pages/ErrorNotFound.vue')
+        .then((errorNotFound) => errorNotFound)
+        .catch(() => (window.location.href = window.location.href)),
   },
 ];
 
