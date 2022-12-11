0e903d1fb75c1e7ed1c16f2664523db7074fc8e0
Luca <lb.luca.bonetti@gmail.com>
2022-12-11 16:02:16 +0100

    refactor: styling

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -15,12 +15,20 @@ import routes from './routes';
  * The function below can be async too; either use
  * async/await or return a Promise which resolves
  * with the Router instance.
+ *
+ * The function defined takes { store, ssrContext } as optional params
  */
 
-export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+export default route(function () {
+  let createHistory;
+  if (process.env.SERVER) createHistory = createMemoryHistory;
+  else {
+    if (process.env.VUE_ROUTER_MODE === 'history') {
+      createHistory = createWebHistory;
+    } else {
+      createHistory = createWebHashHistory;
+    }
+  }
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
--- client/src/stores/index.ts
+++ client/src/stores/index.ts
@@ -1,5 +1,5 @@
-import { store } from 'quasar/wrappers'
-import { createPinia } from 'pinia'
+import { store } from 'quasar/wrappers';
+import { createPinia } from 'pinia';
 
 /*
  * If not building with SSR mode, you can
@@ -8,13 +8,15 @@ import { createPinia } from 'pinia'
  * The function below can be async too; either use
  * async/await or return a Promise which resolves
  * with the Store instance.
+ *
+ * The lambda function defined in the store takes { ssrContext } as optional param
  */
 
-export default store((/* { ssrContext } */) => {
-  const pinia = createPinia()
+export default store(() => {
+  const pinia = createPinia();
 
   // You can add Pinia plugins here
   // pinia.use(SomePiniaPlugin)
 
-  return pinia
-})
+  return pinia;
+});
