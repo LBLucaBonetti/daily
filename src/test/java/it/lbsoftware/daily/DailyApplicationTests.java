9fc45a7b3158aa1acad8ad4aa658ebc3a30f03c9
Luca <lb.luca.bonetti@gmail.com>
2023-01-08 15:47:08 +0100

    test: add security beans case

--- src/test/java/it/lbsoftware/daily/DailyApplicationTests.java
+++ src/test/java/it/lbsoftware/daily/DailyApplicationTests.java
@@ -5,13 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("DailyApplication integration tests")
-class DailyApplicationIntegrationTests extends DailyAbstractIntegrationTests {
+@DisplayName("DailyApplication tests")
+class DailyApplicationTests {
 
   @Test
   @DisplayName("Should not throw when the application starts")
   void test1() {
-    DailyApplication.main(new String[] {"--spring.profiles.active=test,oauth2"});
+    DailyApplication.main(
+        new String[] {
+          "--spring.profiles.active=test,oauth2",
+          "--daily.cookie.csrf.secure=true",
+          "--daily.cookie.csrf.same-site=STRICT"
+        });
     assertTrue(true);
   }
 }
