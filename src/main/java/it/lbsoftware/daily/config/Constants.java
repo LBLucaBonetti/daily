bbe6e6214cb483f7516d0f1988e5d65a57c8e97d
Luca <lb.luca.bonetti@gmail.com>
2023-01-22 21:07:33 +0100

    feat: add cache names and basic spel

--- src/main/java/it/lbsoftware/daily/config/Constants.java
+++ src/main/java/it/lbsoftware/daily/config/Constants.java
@@ -10,6 +10,10 @@ public final class Constants {
   public static final String DAILY_COOKIE_CSRF_SAME_SITE_KEY = "daily.cookie.csrf.same-site";
   public static final String ERROR_KEY = "error";
   public static final String ERROR_DEFAULT = "error.default";
+  public static final String BASIC_SINGLE_ENTITY_CACHE_KEY_SPEL =
+      "'appUser:' + #appUser + ':' + #uuid";
+  public static final String TAG_CACHE = "tag";
+  public static final String NOTE_CACHE = "note";
   public static final int NOTE_TEXT_MAX = 255;
   public static final int TAG_NAME_MAX = 31;
   public static final int NOTE_TAGS_MAX = 5;
