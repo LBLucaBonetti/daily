7eefb61358dac719915649901568a1736e04d090
Luca <lb.luca.bonetti@gmail.com>
2023-04-05 21:32:10 +0200

    chore: remove unused list converters for dtos

--- src/main/java/it/lbsoftware/daily/tags/TagDtoMapper.java
+++ src/main/java/it/lbsoftware/daily/tags/TagDtoMapper.java
@@ -1,6 +1,5 @@
 package it.lbsoftware.daily.tags;
 
-import java.util.List;
 import java.util.Set;
 import org.mapstruct.Mapper;
 
@@ -10,8 +9,6 @@ public interface TagDtoMapper {
   Tag convertToEntity(TagDto tagDto);
 
   TagDto convertToDto(Tag tag);
-
-  List<TagDto> convertToDto(List<Tag> tags);
 
   Set<TagDto> convertToDto(Set<Tag> tags);
 }
