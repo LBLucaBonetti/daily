447ea5b67a3c73bdc8744f0299d4822383e320e7
Luca <lb.luca.bonetti@gmail.com>
2024-05-04 12:12:43 +0200

    feat: add methods to remove data regarding the app user

--- src/main/java/it/lbsoftware/daily/appusersettings/AppUserSettingRepository.java
+++ src/main/java/it/lbsoftware/daily/appusersettings/AppUserSettingRepository.java
@@ -3,8 +3,15 @@ package it.lbsoftware.daily.appusersettings;
 import it.lbsoftware.daily.appusers.AppUser;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AppUserSettingRepository extends JpaRepository<AppUserSetting, Long> {
 
   Optional<AppUserSetting> findByAppUser(AppUser appUser);
+
+  @Query("delete from AppUserSetting aus where aus.appUser = :appUser")
+  @Modifying
+  void deleteByAppUser(@Param("appUser") AppUser appUser);
 }
