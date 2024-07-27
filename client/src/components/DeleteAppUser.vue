e92e191fdfe924ff70d77a26f65e1dd2a75f8709
Luca <lb.luca.bonetti@gmail.com>
2024-07-27 17:14:08 +0200

    feat: add temporarily-disabled delete account component

---
+++ client/src/components/DeleteAppUser.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-btn
+    disable
+    color="red-8"
+    unelevated
+    :label="$t('settings.dangerZone.deleteAppUser.button')"
+    aria-label="Delete account"
+  ></q-btn>
+</template>
+<script setup lang="ts">
+import { QBtn } from 'quasar';
+</script>
